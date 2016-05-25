cb854f94a2083b00d1a61cafc898df4053782172
binlanlua <binlaniua@gmail.com>
2016-05-25 22:47:48 +0800

    add db

--- random/util.go
+++ random/util.go
@@ -1,4 +1,65 @@
 package random
 
+import (
+	"math/rand"
+	"time"
+)
+
+//-------------------------------------
+//
+//
+//
+//-------------------------------------
+func do(size int, kind int) string {
+	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+	is_all := kind > 2 || kind < 0
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		if is_all {
+			// random ikind
+			ikind = rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base + rand.Intn(scope))
+	}
+	return string(result)
+}
+
+//-------------------------------------
+//
+// 
+//
+//-------------------------------------
+func RandomNum(size int) string {
+	return do(size, 0)
+}
+
+//-------------------------------------
+//
+//
+//
+//-------------------------------------
+func RandomLowString(size int) string {
+	return do(size, 1)
+}
+
+//-------------------------------------
+//
+//
+//
+//-------------------------------------
+func RandomUpperString(size int) string {
+	return do(size, 2)
+}
+
+
+//-------------------------------------
+//
+//
+//
+//-------------------------------------
+func RandomString(size int) string {
+	return do(size, 3)
+}
 
 
