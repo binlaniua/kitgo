b8aad77c58eb6aad25fafd2033799d3ea00832f5
Tkk <tangkun@qianmi.com>
2016-06-15 18:17:45 +0800

    修复 http 下 gzip 读取失败

--- slice.go
+++ slice.go
@@ -1,15 +1,2 @@
 package kitgo
 
-//-------------------------------------
-//
-//
-//
-//-------------------------------------
-func ArrayRemoveAtIndex(src []interface{}, index int) interface{} {
-	if len(src) > index {
-		end := index + 1
-		return append(src[:index], src[end:]...)
-	} else {
-		return src
-	}
-}
