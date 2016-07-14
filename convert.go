ee1d59245dd210e09d94440bec51005422216a9e
Tkk <tangkun@qianmi.com>
2016-07-14 17:45:44 +0800

    增加toFixed方法

--- convert.go
+++ convert.go
@@ -87,7 +87,7 @@ func ConvertMustFloat(src string) float64 {
 //
 //
 //-------------------------------------
-func toFixed(src float64, n int) float64 {
+func ToFixed(src float64, n int) float64 {
 	pow10_n := math.Pow10(n)
 	return math.Trunc((src / pow10_n) * pow10_n) / pow10_n
 }
