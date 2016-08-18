19ca612b800a1a713d9a45a3449e6ddc40ee831c
Binlaniua <Binlaniua@gmail.com>
2016-08-18 20:50:30 +0800

    修复bug

--- security/md5.go
+++ security/md5.go
@@ -35,10 +35,10 @@ func MD5WithSalt(s string, salt string) string {
 // 有序MD5加密
 //
 //-------------------------------------
-func MD5Map(m map[string]string, joinStr string, other ... string) string {
+func MD5MapExt(m map[string]string, keyValJon string, joinStr string, other ... string) string {
 	//1 排序
 	kList := make([]string, 0, len(m))
-	for k, _ := range m {
+	for k := range m {
 		kList = append(kList, k)
 	}
 	sort.Strings(kList)
@@ -46,14 +46,15 @@ func MD5Map(m map[string]string, joinStr string, other ... string) string {
 	//2 按 = 号相加
 	strList := make([]string, 0, len(m))
 	for _, k := range kList {
-		v := fmt.Sprintf("%s=%s", k, m[k])
+		v := fmt.Sprintf("%s%s%s", k, keyValJon, m[k])
 		strList = append(strList, v)
 	}
+
 
 	//3 加额外的
 	allStr := strings.Join(strList, joinStr)
 	if other != nil {
-		for _, otherItem := range other  {
+		for _, otherItem := range other {
 			allStr = allStr + otherItem
 		}
 	}
@@ -61,5 +62,9 @@ func MD5Map(m map[string]string, joinStr string, other ... string) string {
 	//4. 大写加密
 	r := strings.ToUpper(MD5(allStr))
 	return r
+}
+
+func MD5Map(m map[string]string, joinStr string, other ... string) string {
+	return MD5MapExt(m, "=", joinStr, other...)
 }
 
