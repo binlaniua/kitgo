65c61cbbd293c3cd1c3b9a06920fdccf99987639
tkk <binlaniua@gmail.com>
2016-10-17 17:36:16 +0800

    增加填充模式

--- security/aes.go
+++ security/aes.go
@@ -3,6 +3,7 @@ package security
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"bytes"
 )
 
 
@@ -39,4 +40,26 @@ func AESDecrypt(key []byte, src []byte) (strDesc string, err error) {
 	aesDecrypter := cipher.NewCFBDecrypter(aesBlockDecrypter, iv)
 	aesDecrypter.XORKeyStream(decrypted, src)
 	return string(decrypted), nil
+}
+
+//-------------------------------------
+//
+// 
+//
+//-------------------------------------
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+//-------------------------------------
+//
+// 
+//
+//-------------------------------------
+func PKCS5UnPadding(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+	return src[:(length - unpadding)]
 }
