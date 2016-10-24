1900b226ac31ed874d3c7d9dc7d606cc91f548b7
tkk <binlaniua@gmail.com>
2016-10-24 19:16:51 +0800

    增加运行日志

--- redis.go
+++ redis.go
@@ -1,14 +1,15 @@
 package kitgo
 
-import "github.com/garyburd/redigo/redis"
-
+import (
+	"github.com/garyburd/redigo/redis"
+)
 
 //-------------------------------------
 //
 //
 //
 //-------------------------------------
-func RedisLrange(redisConn redis.Conn, key string, start int, end int) ([]string, error)  {
+func RedisZrange(redisConn redis.Conn, key string, start int, end int) ([]string, error)  {
 	r, err := redisConn.Do("ZRANGE", key, start, end)
 	if err != nil {
 		return nil, err
@@ -19,4 +20,35 @@ func RedisLrange(redisConn redis.Conn, key string, start int, end int) ([]string
 		result = append(result, string(item.([]byte)))
 	}
 	return result, nil
+}
+
+//-------------------------------------
+//
+//
+//
+//-------------------------------------
+func RedisLrange(redisConn redis.Conn, key string, start int, end int) ([]string, error)  {
+	r, err := redisConn.Do("LRANGE", key, start, end)
+	if err != nil {
+		return nil, err
+	}
+	rs := r.([]interface{})
+	result := []string{}
+	for _, item := range rs {
+		result = append(result, string(item.([]byte)))
+	}
+	return result, nil
+}
+
+//
+//
+//
+//
+//
+func RedisLLen(rc redis.Conn, key string) (int64, error)  {
+	r, err := rc.Do("LLEN", key)
+	if err != nil {
+		return -1, err
+	}
+	return r.(int64), nil
 }
